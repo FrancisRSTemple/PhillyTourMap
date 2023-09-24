c73d898c7fc0fd9b7604c9efbb2e56071c6ff73b
Francis R Scallatino <tup31915@temple.edu>
2023-09-24 08:52:31 -0400

    add image of map

--- philly_tour_map/src/App.js
+++ philly_tour_map/src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
 import Title from './components/Title.jsx';
+import map from './resources/images/map.png';
 
 function App() {
-
   return (
     <div className="App">
       <header className="App-header">
         <Title>Philly Tour Map</Title>
       </header>
+
+      <h2 className='Map'>My Image</h2>
+      <img src={map} alt="Map"/>
     </div>
   );
 }
